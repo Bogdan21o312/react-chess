f3233cb69b09bd6b22de4e2dad58b7a7d72908fa
Bogdan21o312 <lutskiv2010@gmail.com>
2022-12-10 01:06:43 +0200

    Updated index.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {testAPI} from "../../sevices/test";
 import classes from "./Home.module.scss"
 import Layout from "../../components/@UI/Layout";
+import BoardComponents from "../../components/Board/BoardComponents";
+import {Board} from "../../models/Board";
 
 const Index = () => {
     const {data: test, isLoading, error} = testAPI.useFetchAllTestQuery('')
+    const [board, setBoard] = useState(new Board())
+    useEffect(() => {
+        restart()
+    }, [])
+
+    const restart = () => {
+        const newBoard = new Board()
+        newBoard.initCells()
+        setBoard(newBoard)
+    }
 
     return (
         <Layout>
-            {isLoading && <h1>Loading...</h1>}
-            {error && <h2>Error...</h2>}
-            {test && test.map(test =>
-                <div key={test.id}>
-                    {test.id}
-                    {test.body}
-                </div>
-            )}
+            {/*{isLoading && <h1>Loading...</h1>}*/}
+            {/*{error && <h2>Error...</h2>}*/}
+            {/*{test && test.map(test =>*/}
+            {/*    <div key={test.id}>*/}
+            {/*        {test.id}*/}
+            {/*        {test.body}*/}
+            {/*    </div>*/}
+            {/*)}*/}
+
+            <BoardComponents
+                board={board}
+                setBoard={setBoard}
+            />
 
         </Layout>
     );
